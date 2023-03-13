ff186fcc3e9345d5c4018416bebbb8edc35908d5
Ayman Faghloumi <ayman.faghloumi@etu.univ-grenoble-alpes.fr>
2023-03-13 11:31:01 +0100

    UPDATE commenting the EditionMap code

--- Visit360/main/js/EditionMap.js
+++ Visit360/main/js/EditionMap.js
@@ -1,18 +1,22 @@
-//Get the updated list of images when we're on that we stored when leaving on edition Mode
+//-------------------------------------------------------------------------------------
+//List of panoramical images
+//-------------------------------------------------------------------------------------
+
+//Get the updated list of images that we stored when leaving the Edition Mode page
 const myVariableList = localStorage.getItem('imagesDiv');
+//If this list is found in localStorage, then copy it into image-list div
+//Else, set new the initial list of images 
 if (myVariableList) {
     const imagesDiv = document.querySelector('#image-list');
     imagesDiv.innerHTML = myVariableList;
 }
+//ADD a red bar on each image because it wasn't added to the visit yet
 else {
-    //ADD a small circle on each image
-    //We add them so that we see the small circles if we load the miniMap before starting Edition
-
     const imageList = document.getElementById("image-list")
     // Get all the img elements on the imageList
     const images = imageList.getElementsByTagName('img');
 
-    // Loop through each image and add a circle to its top left corner
+    // Loop through each image and add a bar to its left side
     for (let i = 0; i < images.length; i++) {
         const img = images[i];
         const circle = document.createElement("circle");
@@ -27,7 +31,7 @@ else {
     }
 }
 
-//Select one image
+//Select one image from the list
 let selectedImage = null;
 function selectImage(imageUrl) {
     if (selectedImage) {
@@ -46,16 +50,21 @@ function selectImage(imageUrl) {
 //-------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------
 
+//If the correspondant miniMap has already been created, then get it from local Storage 
 const miniMap = document.getElementById("miniMap");
 const myVariableMiniMap = localStorage.getItem('miniMapDiv');
 if (myVariableMiniMap) {
     miniMap.innerHTML = myVariableMiniMap;
 }
-//const modeButton = document.getElementById("mode-button");
+
+//input form filled y the user
 const addAssociatedSceneForm = document.getElementById("add-scene-form")
+//Name of the associated scene
 const sceneAssociated = document.getElementById("sceneAssociated");
+//Button to submit the name of the scene associated
 const addSceneOnMApButton = document.getElementById("add-scene-map")
 
+//List of names of the scenes that have been added in the Edition Mode
 const listOfScene = document.getElementById("listOfScene")
 const myOldSceneDiv = localStorage.getItem('sceneDiv');
 if (myOldSceneDiv) {
@@ -87,10 +96,13 @@ searchInput.addEventListener('input', () => {
     });
 });
 
+//The last created circle on the miniMap
 var lastCreatedCircle = null
 
+//Listener on the button Add scene to the mini Map
 addSceneOnMApButton.addEventListener("click", function(e, arg){
     if(lastCreatedCircle){
+        //Associate the last created circle to the correspondant scene
         lastCreatedCircle.setAttribute("name", sceneAssociated.value)
     }
     searchInput.value=""
@@ -102,6 +114,7 @@ addSceneOnMApButton.addEventListener("click", function(e, arg){
     });
 })
 
+//Function to create a circle
 function createCircle(){
     const circle = document.createElement("circle");
     circle.style.width = "2.5%";
@@ -111,30 +124,33 @@ function createCircle(){
     circle.style.zIndex = 1;
     return circle;
 }
+
+//Function called by the listener on the mini Map div to create a circle with a responsive size
+//And set a Deletion listener on the created circle
 var mapClickHandler = function(event) {
         var circle = createCircle();
         circle.style.backgroundColor = "red";
         circle.style.left = ((event.clientX - miniMap.getBoundingClientRect().left)/miniMap.clientWidth)*100 + "%";
         circle.style.top = ((event.clientY - miniMap.getBoundingClientRect().top)/miniMap.clientHeight)*100 + "%";
-        //circle.style.left = event.clientX + "px";
-        //circle.style.top = event.clientY + "px";
         miniMap.appendChild(circle);
         setDeletionListener(circle);
         lastCreatedCircle = circle
+        //Show the div to set a name to the created circle
         addAssociatedSceneForm.style.display = "block";
 };
+
 var Hovercircle = createCircle();
+//Set a hover circle to help the user set the circle in the desired position
 var mapHoverHandler = function(event){
     Hovercircle.style.backgroundColor = "red";
     Hovercircle.style.left = ((event.clientX - miniMap.getBoundingClientRect().left)/miniMap.clientWidth)*100 + "%";
     Hovercircle.style.top = ((event.clientY - miniMap.getBoundingClientRect().top)/miniMap.clientHeight)*100 + "%";
-    //Hovercircle.style.left = event.clientX + "px";
-    //Hovercircle.style.top = event.clientY + "px";
     Hovercircle.style.opacity = 0.5;
     Hovercircle.setAttribute("class", "hover-circle");
     miniMap.appendChild(Hovercircle)
 };
 
+//Set the map click handler and the map hover handler
 function setMapListener(){
     miniMap.addEventListener("click", mapClickHandler);
     miniMap.addEventListener("mousemove", mapHoverHandler);
@@ -142,17 +158,20 @@ function setMapListener(){
 
 var lastSelectedCircle;
 
+//Delete the targetted circle
 function deletionHandler(event) {
     event.preventDefault();
     event.target.remove();
 }
 
+//Call the deletionHandler function to delete the circle when a right click on it is detected  
 function setDeletionListener(circle) {
     if (circle) {
         circle.addEventListener('contextmenu', deletionHandler);
     }
 }
 
+//Get all the circles on the miniMap and set a DEeletion Listener for each one of them 
 function setDeletionListenerOnAll(){
     var circles = miniMap.querySelectorAll("circle");
     if(circles){
@@ -160,10 +179,12 @@ function setDeletionListenerOnAll(){
     }
 }
 
-mode = "Edition";
+//Initial configuration
 setMapListener();
 setDeletionListenerOnAll()
 
+//Code to execute when leaving the miniMap Edition Mode
+//Store the current content of the miniMap
 window.addEventListener('beforeunload', function (event) {
     var tempMiniMap = miniMap.cloneNode(true);
     tempMiniMap.getElementsByClassName("hover-circle")[0].remove()
@@ -172,6 +193,7 @@ window.addEventListener('beforeunload', function (event) {
     localStorage.setItem('miniMapDiv', miniMapDiv);
 });
 
+//Function called to load the Edition Mode page 
 function toggleMode(){
     window.location="../html/index.html"
 }
